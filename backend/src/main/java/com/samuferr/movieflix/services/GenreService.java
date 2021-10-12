cdb1c9b80548c97b8160633d82ece0f9ab82c41f
Samuel Alves Lima Ferreira <samuel@Samuels-MacBook-Air.local>
2021-10-12 16:20:35 -0300

    DTO

--- backend/src/main/java/com/samuferr/movieflix/services/GenreService.java
+++ backend/src/main/java/com/samuferr/movieflix/services/GenreService.java
@@ -1,11 +1,13 @@
 package com.samuferr.movieflix.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.samuferr.movieflix.dto.GenreDTO;
 import com.samuferr.movieflix.entities.Genre;
 import com.samuferr.movieflix.repositories.GenreRepository;
 
@@ -16,7 +18,9 @@ public class GenreService {
 	private GenreRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Genre> findAll(){
-		return repository.findAll();
+	public List<GenreDTO> findAll(){
+		List<Genre> list = repository.findAll();
+		
+		return list.stream().map(x -> new GenreDTO(x)).collect(Collectors.toList());
 	}
 }
