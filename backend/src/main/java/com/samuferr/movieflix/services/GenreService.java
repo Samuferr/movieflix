5b53d7eaa5a36d5e4cd32fbfe5e27a46f1c95f49
Samuel Alves Lima Ferreira <samuel@Samuels-MacBook-Air.local>
2021-10-12 16:07:14 -0300

    Database seeding

--- backend/src/main/java/com/samuferr/movieflix/services/GenreService.java
+++ backend/src/main/java/com/samuferr/movieflix/services/GenreService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.samuferr.movieflix.entities.Genre;
 import com.samuferr.movieflix.repositories.GenreRepository;
@@ -14,6 +15,7 @@ public class GenreService {
 	@Autowired
 	private GenreRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Genre> findAll(){
 		return repository.findAll();
 	}
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_genre (name) VALUES ('Ação');
+INSERT INTO tb_genre (name) VALUES ('Animação');
+INSERT INTO tb_genre (name) VALUES ('Aventura');
+INSERT INTO tb_genre (name) VALUES ('Drama');
+INSERT INTO tb_genre (name) VALUES ('Ficção científica');
+INSERT INTO tb_genre (name) VALUES ('Terror');
