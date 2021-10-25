99ef45c7c93e36bbae8bb098d5c91dfb4cc8cde6
Samuel Alves Lima Ferreira <samuel@Samuels-MacBook-Air.local>
2021-10-25 11:11:36 -0300

    Fix: Remove user roles on constructor

--- backend/src/main/java/com/samuferr/movieflix/entities/User.java
+++ backend/src/main/java/com/samuferr/movieflix/entities/User.java
@@ -48,12 +48,11 @@ public class User implements Serializable {
 		
 	}
 
-	public User(Long id, String name, String email, String password, Set<Role> roles) {
+	public User(Long id, String name, String email, String password) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.password = password;
-		this.roles = roles;
 	}
 
 	public Long getId() {
@@ -90,10 +89,6 @@ public class User implements Serializable {
 
 	public Set<Role> getRoles() {
 		return roles;
-	}
-
-	public void setRoles(Set<Role> roles) {
-		this.roles = roles;
 	}
 	
 	public List<Review> getReviews() {
