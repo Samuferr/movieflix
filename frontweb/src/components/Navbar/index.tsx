eb01e8a05f4bead04c8b38968ef2809c2387c61b
Samuel Alves Lima Ferreira <samuel@Samuels-MacBook-Air.local>
2021-11-01 12:06:48 -0300

    Navbar created

---
+++ frontweb/src/components/Navbar/index.tsx
@@ -0,0 +1,15 @@
+
+function Navbar() {
+    return (
+        <nav className="bg-primary">
+        <div>
+          <a href="link">
+            <h4>Movieflix</h4>
+          </a>
+        </div>
+      </nav>
+      
+    );
+  }
+  
+  export default Navbar;
