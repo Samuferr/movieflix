db78567108cd6985d41ffe83ee150bfef017ccb8
Samuel Alves Lima Ferreira <samuel@Samuels-MacBook-Air.local>
2021-11-12 09:11:19 -0300

    MovieDetails Html

--- frontweb/src/pages/MovieDetails/index.tsx
+++ frontweb/src/pages/MovieDetails/index.tsx
@@ -1,7 +1,25 @@
 const MovieDetails = () => {
-    return (
-        <h1>Movie Details Page</h1>
-    )
-}
+  return (
+    <div className="details-container container">
+      <div className="row">
+        <div className="col-xl-6">
+          <div className="img-container">
+              <img src="https://www.themoviedb.org/t/p/w533_and_h300_bestv2/n6bUvigpRFqSwmPp1m2YADdbRBc.jpg" alt="Movie Name" />
+          </div>
+        </div>
+        <div className="col-xl-6">
+          <div className="title-subtitle-container">
+            <h1>O Retorno do Rei</h1>
+            <h6>2013</h6>
+            <p>O olho do inimigo está se movendo.</p>
+          </div>
+          <div className="sinopse-container">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste est ex similique optio, nostrum quam debitis necessitatibus cum tenetur saepe architecto? Omnis distinctio quasi assumenda dicta, ipsam perspiciatis sequi illo!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default MovieDetails;
